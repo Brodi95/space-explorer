37ef3f063cd3d1484d836f2865d671d671ed97e4
Rodion <rodion.gamaley@web.de>
2018-06-02 15:48:48 +0200

    Player Shooting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private const float maxDistance = 4f;
+    private const float speed = 5f;
+
+    public Vector2 startPos;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private bool shoot;
+
+    public Vector2 Direction
+    {
+        get
+        {
+            return direction;
+        }
+
+        set
+        {
+            direction = value;
+            shoot = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!shoot)
+            return;
+
+        if (Vector2.Distance(startPos, transform.position) >= maxDistance)
+        {
+            Debug.Log("die");
+            Destroy(gameObject);
+        }
+
+        Vector3 moveDirection = Direction * speed * Time.fixedDeltaTime;
+        transform.position += moveDirection;
+    }
+
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public GameObject BulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if(x == 0 && y == 0)
+        {
+            x = Player.player.facingRight ? 1 : -1;
+        }
+
+        var bullet = Instantiate(BulletPrefab).GetComponent<Bullet>();
+        bullet.startPos = transform.position;
+        bullet.Direction = new Vector2(x, y);
+    }
+}
