97871397ee6b14e84458de025e8dd7519c87d2b6
John <John.peter.hansen@live.de>
2018-06-02 14:06:15 +0200

    Alien Can Attack now

---
+++ Assets/AlienAttacks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienAttacks : MonoBehaviour {
+
+    [Header("Attacks")]
+    public GameObject fireRing;
+    public GameObject alienSpit;
+    
+
+    public void FireAttack(GameObject target)
+    {
+        Instantiate(fireRing, transform.position, Quaternion.identity);
+    }
+
+    public void SpitAttack(GameObject target)
+    {
+        GameObject temp = Instantiate(alienSpit, transform.position, Quaternion.identity);
+        temp.GetComponent<AlienSpit>().Fire(target.transform.position - transform.position, 2);
+    }
+
+
+}
